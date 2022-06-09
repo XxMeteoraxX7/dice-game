e1136042fa7fd1c2febde187da3cdff4cc7a9e8e
Adam S. Eidson <104181029+XxMeteoraxX7@users.noreply.github.com>
2022-06-08 23:47:23 -0500

    Update dice-game.js

--- dice-game.js
+++ dice-game.js
@@ -2,12 +2,15 @@ function rollDice() {
     let goldCoins = 0;
     for (let i = 0; i < 10; i++) {
         const roll = Math.floor(Math.random() * 6) + 1;
+        let doubleBet Math.floor(Math.random() * 6) + 1;
         alert('You have accumulated a total of ' + goldCoins + ' gold coins so far.\n\nYou rolled a ' + roll + '!');
         if (roll === 1) {
             alert('Game over, no more rolls!');
             break;
         }
-        if (roll < 4) {
+        if (roll < 4 && doubleBet > 3) {
+            prompt('You rolled a lucky roll!\n\nYou can choose to double or nothing waging the next roll will be greater than 3.\n\nEnter 1 to accept, or 0 to skip.');
+            
             continue;
         }
         if (roll === 4 && goldCoins > 0) {
